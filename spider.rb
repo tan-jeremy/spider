0b985f3808d9cae571cc20bad162849f5add77b0
F227 <user@F227-32.(none)>
2014-04-30 23:25:55 +0800

    made some changes

--- spider.rb
+++ spider.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
 get '/hi' do
-	"It's The Amazing Spider-Man 2's premiere!"
+	puts "It's The Amazing Spider-Man 2's premiere!"
+end
+
+get '/index' do
+	"<h1>April 30, 2014 - Amazing Spider-Man 2 premiere</h1>"
 end
